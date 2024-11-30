23bb3519b01248a0a6a4f688662ec0ce14f95b8c
Yashwanthdevulapally <yashwanthdevulapally1@gmail.com>
2024-11-30 13:28:06 +0530

    Delete diabetescheck directory

--- diabetescheck/urls.py
+++
@@ -1,8 +0,0 @@
-# diabetescheck/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('predictor.urls')),
-]
